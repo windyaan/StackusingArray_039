7417d87e47a8ca60339d5238fb9596910e4fa15b
windyaan <windyanurmaretha@gmail.com>
2023-06-15 12:41:10 +0700

    Fungsi utama, melakukan perulangan

--- StackUsingArray/StackUsingArray.cpp
+++ StackUsingArray/StackUsingArray.cpp
@@ -53,3 +53,21 @@ public:
 		}
 	}
 };
+
+int main() {
+	Program s;
+
+	while (true) {
+		cout << endl;
+		cout << "\n***Stack Menu***\n";
+		cout << "1. Push\n";
+		cout << "2. Pop\n";
+		cout << "3. Display\n";
+		cout << "4. Exit\n";
+		cout << "\nEnter your choice: ";
+		string input;
+		getline(cin, input);
+		char ch = (input.empty() ? '0' : input[0]);
+		
+	}
+}

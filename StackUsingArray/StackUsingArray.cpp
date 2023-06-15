dd44e1fe1fdca5fcea858f12ff61cc6278818476
asus <asus@LAPTOP-DAGQJ192>
2023-06-15 10:49:26 +0700

    Add project files.

---
+++ StackUsingArray/StackUsingArray.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Program {
+private:
+	string stack_array[5];
+	int top;
+
+public:
+	Program() {
+		top = -1;
+	}
+
+	string push(string element) {
+		if (top == 4) {
+			cout << "Stack Full." << endl;
+			return "";
+		}
+
+		top++;
+		stack_array[top] = element;
+
+		cout << endl;
+		cout << element << "ditambahkan (pushed)." << endl;
+
+		return element;
+	}
+
+	void pop() {
+		if (empty()) {
+			cout << "\nStack is empty. Cannot pop." << endl;
+			return;
+		}
+
+		cout << "\nThe popped element is: " << stack_array[top] << endl;
+		top--;
+	}
+
+	bool empty() {
+		return (top == -1);
+	}
+
+	void display() {
+		if (empty()) {
+			cout << "\nStack is empty." << endl;
+		}
+		else {
+			for (int top = top; top >= 0; top--) {
+				cout << stack_array[top] << endl;
+			}
+		}
+	}
+};
